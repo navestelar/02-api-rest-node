0e9c8e7659bd5852b2a9aa41e84452ce3b5e69e9
navestelar <acch.escola@gmail.com>
2025-10-08 22:36:02 -0300

    chore: transactions tests

--- src/env/index.ts
+++ src/env/index.ts
@@ -4,14 +4,13 @@ import { z } from 'zod'
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
   DATABASE_URL: z.string(),
-  PORT: z.number().default(3333),
+  PORT: z.coerce.number().default(3333),
 })
 
 const _env = envSchema.safeParse(process.env)
 
-if (_env.success === false) {
+if (!_env.success) {
   console.error('⚠️ Invalid environment variables', _env.error.format())
-
   throw new Error('Invalid environment variables.')
 }
 
